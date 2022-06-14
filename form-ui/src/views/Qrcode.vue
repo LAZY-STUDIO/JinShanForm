04021468bc663a0b374ec79633510e103b415f47
周蔚然 <2544795750@qq.com>
2022-06-14 17:17:53 +0800

    添加anayse路由

--- form-ui/src/views/Qrcode.vue
+++ form-ui/src/views/Qrcode.vue
@@ -2,7 +2,7 @@
   <div class="qrcodes">
     <QRcode :id="ids"></QRcode>
     <div class="download"><a class="link" @click="down()">下载二维码</a></div>
-    <div class="copy" @click="copyLink('datanayse')">复制链接</div>
+    <div class="copy" @click="copyLink('write')">复制链接</div>
   </div>
 </template>
 
@@ -18,7 +18,7 @@ export default defineComponent({
   methods: {
     copyLink(val: string) {
       console.log(val, '复制链接')
-      let url = window.location.origin + '/' + val
+      let url = window.location.origin + '/' + val + '?id=' + this.ids
       let inputNode = document.createElement('input')
       inputNode.value = url
       document.body.appendChild(inputNode)
